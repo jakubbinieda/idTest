f0a9ad193d51b86bf8ff4167f00fd9070ebedf38
Jakub Binięda <jakub.binieda@icloud.com>
2022-05-10 14:39:04 +0200

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Ten no ID czy cos" });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+  
